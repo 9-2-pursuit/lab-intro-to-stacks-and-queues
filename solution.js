19cd04976dc223d5eca92060e275f828104480d2
isaacguerrero <isaacguerrero@pursuit.org>
2023-03-29 11:32:49 -0400

    Add Node, Stack and Queue class, and push, size, and pop methods for Stack class

--- solution.js
+++ solution.js
@@ -1,6 +1,54 @@
 const { nums, words } = require("./data/data.js");
 const { inspect } = require("util");
 
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(top) {
+    this.top = top || null;
+  }
+
+  push(data) {
+    let newItem = new Node(data);
+    if (this.top === null) {
+      this.top = newItem;
+    } else {
+      newItem.next = this.top;
+      this.top = newItem;
+    }
+  }
+
+  size() {
+    let count = 0;
+    let node = this.top;
+    while (node) {
+      node = node.next;
+      count++;
+    }
+    return count;
+  }
+
+  pop() {
+    let popped = this.top;
+    this.top = popped.next;
+    return popped;
+  }
+}
+
+class Queue {
+  constructor(value) {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+    this.max = value;
+  }
+}
+
 module.exports = {
   Node,
   Queue,
